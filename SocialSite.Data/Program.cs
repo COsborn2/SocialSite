60b641430fbde5eff58e6b37eee16578e7abd1eb
Cameron Osborn <cameron0401@gmail.com>
2021-10-21 20:49:13 -0700

    Init

---
+++ SocialSite.Data/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialSite.Data
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // This entry point exists for the EF Core command line tooling.
+        }
+    }
+}
